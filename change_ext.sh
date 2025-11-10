cd09cf65396e72577ade60294b3f846f2b156c94
Галкин Владимир <galkin@irp.ru>
2025-11-10 14:51:00 +0300

    Some changes

--- change_ext.sh
+++ change_ext.sh
@@ -6,11 +6,10 @@ extension=$2
 if [[ "$file" == *"."* ]]; then
   IFS='.' read -ra parts <<< "$file"
   new_file_name=${parts[0]}"."$extension
-  echo "$new_file_name"
+  echo "Файл $file изменен, новый файл $new_file_name"
   mv "$file" "$new_file_name"
 else
     echo "Исходный файл не имеет расширения"
 fi
 
-echo "$file - $extension"
 
